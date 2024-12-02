03acdc5c0cb80876161d68ee4fce9b35eeca22bd
Mariem Elgendy <mariem.elgendy213@gmail.com>
2024-12-02 13:39:40 +0200

    Finish Add Product Section

--- src/components/NewProduct.jsx
+++ src/components/NewProduct.jsx
@@ -1,8 +1,38 @@
 // import React from 'react'
+import Banner from '../assets/orange.jpeg'
+
+const BannerImage = {
+    backgroundImage: `url(${Banner})`,
+    backgroundPosition: 'center',
+    backgroundrepeat: 'no-repeat',
+    backgroundsize: 'cover',
+    height: '100%',
+    width: '100%'
+}
 
 function NewProduct() {
     return (
-        <div>NewProduct</div>
+        <div
+            data-aos = "zoom-in"
+            style={BannerImage}
+            className='
+                bg-gray-100 dark:bg-gray-800
+                text-white 
+                mb-20
+            '
+        >
+            <div className='container backdrop-blur-sm py-10'>
+                <div className='space-y-6 max-w-xl mx-auto'>
+                    <h1 className='text-2xl !text-center sm:text-left sm:text-4xl font-semibold'>Get Notified About New Products</h1>
+                    <input 
+                        type='text'
+                        placeholder='Enter Your Email Address'
+                        className='w-full p-3'
+                        data-aos='fade-up'
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 
